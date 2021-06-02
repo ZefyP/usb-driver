ZefyP/usb-driver

commit 18530b13b6b30eec59382e23544af4d9bda01eb8

Support for concurrent usage of multiple test cards + gcc7 compiler support

USB_a.cc
@@ -1,10 +1,18 @@
 #include "USB_a.h"
 
+
+
+
+
+
+
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%//
+
+//init static members of PSFE
 bool TC_PSROH::is_initialized=false, TC_PSROH::fuse_active=false;
 CP2130 TC_PSROH::cCP2130; 
 std::string TC_PSROH::product_string;
-
-
 
 TC_PSROH::TC_PSROH()
 {
@@ -26,7 +34,37 @@ TC_PSROH::TC_PSROH()
     //%%%configure SPI to I2C chip%%%//
     char buf_IoConfig[11]={0, 0, 1, 0, 3, 0, 0, 0, 0x20, 0x00, 0b10101010}, // All GPIOs Push-Pull
          buf_IoState[11]={0, 0, 1, 0, 3, 0, 0, 0, 0x20, 0x01, 0b00001001}, // All GPIOs to zero
-         buf_I2cClock[11]={0, 0, 1, 0, 3, 0, 0, 0, 0x20, 0x02, 0b00000101}; // max speed I2C, 369KHz
+         buf_I2cClock[11]={0, 0, 1, 0, 3, 0, 0, 0, 0x20, 0x02, 0b00010011}; // max speed I2C, 369KHz
+    cCP2130.choose_spi(cCP2130.cs10);
+    cCP2130.spi_write(buf_IoState,sizeof(buf_IoState));
+    cCP2130.spi_write(buf_IoConfig,sizeof(buf_IoConfig));
+    cCP2130.spi_write(buf_I2cClock,sizeof(buf_I2cClock));
+    //sleep(1);
+    is_initialized=true;
+    }
+}
+
+TC_PSROH::TC_PSROH(uint32_t bus,uint8_t device)
+{
+    if(!is_initialized){
+    cCP2130.initialize(bus,device);
+    product_string.resize(64,' ');
+    cCP2130.get_product_string(&product_string[0]);
+    cCP2130.gpio_set_output(cCP2130.cs9,test_led_state);
+    cCP2130.configure_spi(cCP2130.cs4,cCP2130.SI8902);
+    cCP2130.configure_spi(cCP2130.cs6,cCP2130.ADG731);
+    cCP2130.configure_spi(cCP2130.cs7,cCP2130.MCP4921);
+    cCP2130.configure_spi(cCP2130.cs10,cCP2130.SC18IS600);
+    /////
+    cCP2130.choose_spi(cCP2130.cs4);
+    char buff_s_adc[12] = {0, 0, 2, 0, 4, 0, 0, 0, 0, 0xFF, 0xFF,0xFF}, buff_r_adc[4] ={0}; // com buffers for ADC
+    cCP2130.spi_write (buff_s_adc,sizeof(buff_s_adc));
+    int t_code=cCP2130.spi_read(buff_r_adc,sizeof(buff_r_adc));
+    /////
+    //%%%configure SPI to I2C chip%%%//
+    char buf_IoConfig[11]={0, 0, 1, 0, 3, 0, 0, 0, 0x20, 0x00, 0b10101010}, // All GPIOs Push-Pull
+         buf_IoState[11]={0, 0, 1, 0, 3, 0, 0, 0, 0x20, 0x01, 0b00001001}, // All GPIOs to zero
+         buf_I2cClock[11]={0, 0, 1, 0, 3, 0, 0, 0, 0x20, 0x02, 0b00010011}; // max speed I2C, 369KHz
     cCP2130.choose_spi(cCP2130.cs10);
     cCP2130.spi_write(buf_IoState,sizeof(buf_IoState));
     cCP2130.spi_write(buf_IoConfig,sizeof(buf_IoConfig));
@@ -135,8 +173,8 @@ int TC_PSROH::write_i2c(short int address, char value)
     //cCP2130.spi_write(activate,sizeof(activate));
     cCP2130.spi_write(data,sizeof(data));
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    //return read_i2c(address);
-    return 0;	
+    return read_i2c(address);
+    //return 0;	
 }
 
 int TC_PSROH::read_i2c( short int address)
@@ -215,6 +253,10 @@ int TC_PSROH::dac_output(uint16_t level)
 	return 0;
 }
 
+const char* TC_PSROH::get_product_string()
+{
+return product_string.data();
+}
 
 
 
@@ -232,6 +274,35 @@ TC_PSFE::TC_PSFE()
 {
     if(!is_initialized){
     cCP2130.initialize();
+    product_string.resize(64,' ');
+    cCP2130.get_product_string(&product_string[0]);
+    cCP2130.gpio_set_output(cCP2130.cs1,0);
+    cCP2130.gpio_set_output(cCP2130.cs3,1);
+    cCP2130.gpio_set_output(cCP2130.cs5,0);
+    cCP2130.gpio_set_output(cCP2130.cs7,0);
+    cCP2130.gpio_set_output(cCP2130.cs8,0);
+    cCP2130.gpio_set_output(cCP2130.cs9,0);
+    cCP2130.gpio_set_input(cCP2130.cs10);
+    cCP2130.configure_spi(cCP2130.cs0,cCP2130.ADG714);
+    cCP2130.configure_spi(cCP2130.cs2,cCP2130.AD5231);
+    cCP2130.configure_spi(cCP2130.cs4,cCP2130.SI8902);
+    cCP2130.configure_spi(cCP2130.cs6,cCP2130.ADG731);
+        /////
+    cCP2130.choose_spi(cCP2130.cs4);
+    char buff_s_adc[12] = {0, 0, 2, 0, 4, 0, 0, 0, 0, 0xFF, 0xFF,0xFF}, buff_r_adc[4] ={0}; // com buffers for ADC
+    cCP2130.spi_write (buff_s_adc,sizeof(buff_s_adc));
+    int t_code=cCP2130.spi_read(buff_r_adc,sizeof(buff_r_adc));
+    /////
+    cCP2130.get_gpio_value(cCP2130.cs10,chirality);
+    antenna_fc7(512 , NONE); // default state of antenna
+    is_initialized=true;
+    }
+}
+
+TC_PSFE::TC_PSFE(uint32_t bus,uint8_t device)
+{
+    if(!is_initialized){
+    cCP2130.initialize(bus,device);
     product_string.resize(64,' ');
     cCP2130.get_product_string(&product_string[0]);
     cCP2130.gpio_set_output(cCP2130.cs1,0);
@@ -468,6 +539,11 @@ int TC_PSFE::antenna_fc7(uint16_t pot_value,ant_channel c)
     return 0;
 }
 
+const char* TC_PSFE::get_product_string()
+{
+return product_string.data();
+}
+
 
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%//
@@ -483,6 +559,29 @@ TC_2SFE::TC_2SFE()
 {
     if(!is_initialized){
     cCP2130.initialize();
+    product_string.resize(64,' ');
+    cCP2130.get_product_string(&product_string[0]);
+    cCP2130.gpio_set_output(cCP2130.cs1,0);
+    cCP2130.gpio_set_output(cCP2130.cs3,1);
+    cCP2130.gpio_set_output(cCP2130.cs5,0);
+    cCP2130.configure_spi(cCP2130.cs4,cCP2130.SI8902);
+    cCP2130.configure_spi(cCP2130.cs0,cCP2130.ADG1414);
+    cCP2130.configure_spi(cCP2130.cs2,cCP2130.AD5231);
+    cCP2130.configure_spi(cCP2130.cs3,cCP2130.LTC6903);
+    /////
+    cCP2130.choose_spi(cCP2130.cs4);
+    char buff_s_adc[12] = {0, 0, 2, 0, 4, 0, 0, 0, 0, 0xFF, 0xFF,0xFF}, buff_r_adc[4] ={0}; // com buffers for ADC
+    cCP2130.spi_write (buff_s_adc,sizeof(buff_s_adc));
+    int t_code=cCP2130.spi_read(buff_r_adc,sizeof(buff_r_adc));
+    /////
+    is_initialized=true;
+    }
+}
+
+TC_2SFE::TC_2SFE(uint32_t bus,uint8_t device)
+{
+    if(!is_initialized){
+    cCP2130.initialize(bus,device);
     product_string.resize(64,' ');
     cCP2130.get_product_string(&product_string[0]);
     cCP2130.gpio_set_output(cCP2130.cs1,0);
@@ -567,6 +666,12 @@ int TC_2SFE::adc_get(measurement m, float& output)
 
 }
 
+const char* TC_2SFE::get_product_string()
+{
+return product_string.data();
+}
+
+
 int TC_2SFE::antenna_fc7(uint16_t pot_value,ant_channel c)
 {
     cCP2130.gpio_set_output(cCP2130.cs5,0); //should be default configuration
@@ -629,6 +734,20 @@ CP2130::~CP2130(){
 }
 int CP2130::initialize()
 {
+     if ( ( fUsbHandle = setup_libusb_access() ) == NULL )
+     {
+        std::cout << "Failed to connect with CP2130, check if it is plugged in the USB port." << std::endl;
+        exit ( -1 ); 
+     }
+     get_usb_config();
+     usb_reset ( fUsbHandle ); // MISO doesn't work wihthout this...
+     return 0;
+}
+
+int CP2130::initialize(uint32_t bus,uint8_t device)
+{
+     busnum=(int64_t)bus; 
+     dvcnum=(int16_t)device;     
      if ( ( fUsbHandle = setup_libusb_access() ) == NULL )
      {
         std::cout << "Failed to connect with CP2130, check if it is plugged in the USB port." << std::endl;
@@ -706,6 +825,8 @@ int CP2130::configure_spi(cs_line c, device d)
         int result2 = usb_control_msg (fUsbHandle, 0x40, 0x33, 0, 0, spi_delay, sizeof(spi_delay), fUsbTimeout);
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
+	case ATSAMD51P20A0A_PSPOH:
+	data[1]=0b00101101; break;
         break;
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
@@ -790,7 +911,8 @@ usb_dev_handle* CP2130::find_usb_handle()
         for ( dev = bus->devices; dev; dev = dev->next )
         {
             if ( dev->descriptor.idVendor == VENDOR_ID &&
-                    dev->descriptor.idProduct == PRODUCT_ID )
+                    dev->descriptor.idProduct == PRODUCT_ID 
+			&& ((dev->devnum==dvcnum && bus->location ==busnum)||dvcnum<0))
             {
                 usb_dev_handle* handle;
 

psroh.cc
@@ -1,4 +1,4 @@
-#include <cstring>
+ #include <cstring>
 #include "USB_a.h"
 #include <fstream>
 #include <iostream>
@@ -29,7 +29,141 @@ std::vector<std::pair<int,int>> read_lpgbt_min_config(std::string file)
 int main(){
 
 	TC_PSROH cTC_PSROH;
+	std::cout <<cTC_PSROH.read_i2c(0x020)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x021)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x022)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x023)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x024)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x029)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x02C)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x031)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x035)<<"\n";
+	std::cout <<cTC_PSROH.read_i2c(0x0EF)<<"\n";
 	std::cout <<cTC_PSROH.read_i2c(0x1C7)<<"\n";
+
+	/*
+	auto cRegs = read_lpgbt_min_config("lpgbt_minimal_config.txt");
+	for( auto reg : cRegs )
+	{
+        std::cout <<cTC_PSROH.write_i2c(reg.first,reg.second)<<"\n";
+	}
+	std::cout << "PUSM after def config \n";
+	std::cout <<cTC_PSROH.read_i2c(0x1C7)<<"\n";
+	sleep(10);
+cTC_PSROH.write_i2c(0x110,0xA3);//toggle_2v5 magic number
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control
+	cTC_PSROH.write_i2c(0x10E,0x00); // Address high of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10F,0x20); // Address low of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10A,0xF8); 
+	cTC_PSROH.write_i2c(0x10B,0x24); 
+	cTC_PSROH.write_i2c(0x10C,0x44); 
+	cTC_PSROH.write_i2c(0x10D,0x55);
+	cTC_PSROH.toggle_2v5(); //enable 2v5
+        std::this_thread::sleep_for (std::chrono::milliseconds (1) ); 
+	cTC_PSROH.write_i2c(0x109,0xC1); //toggle_2v5 control on
+	int i=0;
+	while(!(cTC_PSROH.read_i2c(0x1A1)&0b00000010) && i<2 ) {std::cout << "toggle_2v5 in progress\n"; i++;std::cout << cTC_PSROH.read_i2c(0x1A1) << "\n";}
+	cTC_PSROH.toggle_2v5(); //disable 2v5
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control off
+
+	cTC_PSROH.write_i2c(0x10E,0x00); // Address high of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10F,0x24); // Address low of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10A,0x55); 
+	cTC_PSROH.write_i2c(0x10B,0x55); 
+	cTC_PSROH.write_i2c(0x10C,0x55); 
+	cTC_PSROH.write_i2c(0x10D,0x55);
+	cTC_PSROH.toggle_2v5(); //enable 2v5
+        std::this_thread::sleep_for (std::chrono::milliseconds (1) ); 
+	cTC_PSROH.write_i2c(0x109,0xC1); //toggle_2v5 control on
+	i=0;
+	while(!(cTC_PSROH.read_i2c(0x1A1)&0b00000010) && i<2 ) {std::cout << "toggle_2v5 in progress\n"; i++;std::cout << cTC_PSROH.read_i2c(0x1A1) << "\n";}
+	cTC_PSROH.toggle_2v5(); //disable 2v5
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control off
+
+	cTC_PSROH.write_i2c(0x10E,0x00); // Address high of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10F,0x28); // Address low of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10A,0x0F); 
+	cTC_PSROH.write_i2c(0x10B,0x36); 
+	cTC_PSROH.write_i2c(0x10C,0x00); 
+	cTC_PSROH.write_i2c(0x10D,0x00);
+	cTC_PSROH.toggle_2v5(); //enable 2v5
+        std::this_thread::sleep_for (std::chrono::milliseconds (1) ); 
+	cTC_PSROH.write_i2c(0x109,0xC1); //toggle_2v5 control on
+	i=0;
+	while(!(cTC_PSROH.read_i2c(0x1A1)&0b00000010) && i<2 ) {std::cout << "toggle_2v5 in progress\n"; i++;std::cout << cTC_PSROH.read_i2c(0x1A1) << "\n";}
+	cTC_PSROH.toggle_2v5(); //disable 2v5
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control off
+
+	cTC_PSROH.write_i2c(0x10E,0x00); // Address high of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10F,0x2C); // Address low of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10A,0xA0); 
+	cTC_PSROH.write_i2c(0x10B,0x89); 
+	cTC_PSROH.write_i2c(0x10C,0x99); 
+	cTC_PSROH.write_i2c(0x10D,0x0A);
+	cTC_PSROH.toggle_2v5(); //enable 2v5
+        std::this_thread::sleep_for (std::chrono::milliseconds (1) ); 
+	cTC_PSROH.write_i2c(0x109,0xC1); //toggle_2v5 control on
+	i=0;
+	while(!(cTC_PSROH.read_i2c(0x1A1)&0b00000010) && i<2 ) {std::cout << "toggle_2v5 in progress\n"; i++;std::cout << cTC_PSROH.read_i2c(0x1A1) << "\n";}
+	cTC_PSROH.toggle_2v5(); //disable 2v5
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control off
+
+	cTC_PSROH.write_i2c(0x10E,0x00); // Address high of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10F,0x30); // Address low of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10A,0x1A); 
+	cTC_PSROH.write_i2c(0x10B,0x2A); 
+	cTC_PSROH.write_i2c(0x10C,0x3A); 
+	cTC_PSROH.write_i2c(0x10D,0x0A);
+	cTC_PSROH.toggle_2v5(); //enable 2v5
+        std::this_thread::sleep_for (std::chrono::milliseconds (1) ); 
+	cTC_PSROH.write_i2c(0x109,0xC1); //toggle_2v5 control on
+	i=0;
+	while(!(cTC_PSROH.read_i2c(0x1A1)&0b00000010) && i<2 ) {std::cout << "toggle_2v5 in progress\n"; i++;std::cout << cTC_PSROH.read_i2c(0x1A1) << "\n";}
+	cTC_PSROH.toggle_2v5(); //disable 2v5
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control off
+
+	cTC_PSROH.write_i2c(0x10E,0x00); // Address high of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10F,0x34); // Address low of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10A,0x64); 
+	cTC_PSROH.write_i2c(0x10B,0x00); 
+	cTC_PSROH.write_i2c(0x10C,0x80); 
+	cTC_PSROH.write_i2c(0x10D,0x18);
+	cTC_PSROH.toggle_2v5(); //enable 2v5
+        std::this_thread::sleep_for (std::chrono::milliseconds (1) ); 
+	cTC_PSROH.write_i2c(0x109,0xC1); //toggle_2v5 control on
+	i=0;
+	while(!(cTC_PSROH.read_i2c(0x1A1)&0b00000010) && i<2 ) {std::cout << "toggle_2v5 in progress\n"; i++;std::cout << cTC_PSROH.read_i2c(0x1A1) << "\n";}
+	cTC_PSROH.toggle_2v5(); //disable 2v5
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control off
+
+	cTC_PSROH.write_i2c(0x10E,0x00); // Address high of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10F,0x38); // Address low of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10A,0x00); 
+	cTC_PSROH.write_i2c(0x10B,0x40); 
+	cTC_PSROH.write_i2c(0x10C,0x20); 
+	cTC_PSROH.write_i2c(0x10D,0x03);
+	cTC_PSROH.toggle_2v5(); //enable 2v5
+        std::this_thread::sleep_for (std::chrono::milliseconds (1) ); 
+	cTC_PSROH.write_i2c(0x109,0xC1); //toggle_2v5 control on
+	i=0;
+	while(!(cTC_PSROH.read_i2c(0x1A1)&0b00000010) && i<2 ) {std::cout << "toggle_2v5 in progress\n"; i++;std::cout << cTC_PSROH.read_i2c(0x1A1) << "\n";}
+	cTC_PSROH.toggle_2v5(); //disable 2v5
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control off
+
+	cTC_PSROH.write_i2c(0x10E,0x00); // Address high of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10F,0xEC); // Address low of 32bit block to be fused
+	cTC_PSROH.write_i2c(0x10A,0x00); 
+	cTC_PSROH.write_i2c(0x10B,0x00); 
+	cTC_PSROH.write_i2c(0x10C,0x00); 
+	cTC_PSROH.write_i2c(0x10D,0x07);
+	cTC_PSROH.toggle_2v5(); //enable 2v5
+        std::this_thread::sleep_for (std::chrono::milliseconds (1) ); 
+	cTC_PSROH.write_i2c(0x109,0xC1); //toggle_2v5 control on
+	i=0;
+	while(!(cTC_PSROH.read_i2c(0x1A1)&0b00000010) && i<2 ) {std::cout << "toggle_2v5 in progress\n"; i++;std::cout << cTC_PSROH.read_i2c(0x1A1) << "\n";}
+	cTC_PSROH.toggle_2v5(); //disable 2v5
+	cTC_PSROH.write_i2c(0x109,0xC0); //toggle_2v5 control off
+	*/
 	/* ////////////////////////////////////////ADC measurements///////////////////////
 	cTC_PSROH.write_i2c(0x113,0b00000000);
 	cTC_PSROH.write_i2c(0x112,0b00111111);

2sfeh_v1.cc
@@ -9,8 +9,12 @@
 
 
 int main(){
-	TC_2SFE cTC_2SFE;
-	std::cout << cTC_2SFE.product_string << std::endl;
+	TC_2SFE cTC_2SFE(1,32);
+        const char* al=cTC_2SFE.get_product_string();
+	for (int i=0; i<50; i++){
+	std::cout << *(al+i);
+	}
+	std::cout << std::endl;
 	float result;
 	while(1){
 	cTC_2SFE.adc_get(cTC_2SFE.ISEN,result);

USB_a.h
@@ -16,10 +16,11 @@ class CP2130
 {
     public:
     enum cs_line:char{cs0,cs1,cs2,cs3,cs4,cs5,cs6,cs7,cs8,cs9,cs10};
-    enum device:char{ADG731,SI8902,ADG714,ADG1414,AD5231,LTC6903,MCP4921,SC18IS600};
+    enum device:char{ADG731,SI8902,ADG714,ADG1414,AD5231,LTC6903,MCP4921,SC18IS600,ATSAMD51P20A0A_PSPOH};
     CP2130();
     ~CP2130();
     int initialize();
+    int initialize(uint32_t,uint8_t); // initialization for multi usb applications // arguments : bus, device number (lsusb)
     void close();
     int gpio_set_input(cs_line);
     int gpio_set_output(cs_line, bool);
@@ -37,6 +38,8 @@ class CP2130
     usb_dev_handle* fUsbHandle;    
     usb_dev_handle* find_usb_handle();
     usb_dev_handle* setup_libusb_access();
+    int64_t busnum = -1;
+    int16_t dvcnum = -1;
     
     std::string VERSION="0.1.0";
     const static int VENDOR_ID=0x10C4; 
@@ -57,13 +60,12 @@ class TC_2SFE
     enum measurement:char{AMUX,ISEN,THERM_SENSE};
     enum ant_channel:char{NONE,_1,_2,_3,_4,ALL}; // Antenna Options
     TC_2SFE();
+    TC_2SFE(uint32_t,uint8_t); // constructor for multi usb applications // arguments : bus, device number (lsusb) // can be called only once and then revert to the empty one
     ~TC_2SFE();
-    //Card Id// Full after creating a TC_2SFE object
-    static std::string product_string; // Product string
-    //       //
     int adc_get(measurement,float&);
     int toggle_led();   
     int antenna_fc7(uint16_t,ant_channel); //arguments: Potentiometer value, Antenna channel , -2430 + 4.76*dacValue = PULLUP(mV)
+    const char* get_product_string(); // pointer to the ID product
     private:
     static CP2130 cCP2130; // declare CP2130 object
     static char adg714_state; //saved state of adg714, during operation
@@ -72,6 +74,9 @@ class TC_2SFE
     float ADC_VREF=1.25;
     static bool test_led_state;
     const static uint8_t antenna_mask=0x0F; // mask for bits/switches corresponding to antenna channel control - the rest are for voltage control
+    //Card Id// Full after creating a TC_2SFE object
+    static std::string product_string; // Product string
+    //       //
     const static int fTempLookUpTableSize = 34;
     float fTempLookUpTable[fTempLookUpTableSize] = {848, 804, 755, 701, 643, 584, 525, // 848 corresponds to -40 deg C, steps every 5 degree
                                                     467, 413, 362, 315, 273, 236, 203, 175, 150, 129, 111, 95,
@@ -92,13 +97,12 @@ class TC_PSFE
     enum st_mode:char{DISABLED,BUFFER_ONE,POGO_ONE,POGO_ZERO}; // SELF-TEST OPTIONS. FORMAT: SourceLocation_Value
     enum v_control:char{_900mV,_950mV,_1000mV,_1050mV,_1100mV,_1150mV,_1200mV,_1250mV}; // Voltage Control Options
     enum ant_channel:char{NONE,_1,_2,ALL}; // Antenna Options  _2 EVEN, _1 ODD
-    
-    //Card Id// Full after creating a TC_PSFE object
-    static bool chirality; // 0 LEFT, 1 RIGHT
-    static std::string product_string; // Product string
-    //       //
+
+    static bool chirality; // 0 LEFT, 1 RIGHT //full after PSFE object creation
     TC_PSFE();
+    TC_PSFE(uint32_t,uint8_t); // constructor for multi usb applications // arguments : bus, device number (lsusb) // can be called only once and then revert to the empty one
     ~TC_PSFE();
+    const char* get_product_string(); // pointer to the ID product
     int adc_get(measurement,float&); // arguments: measurement choice, float output 
     int set_voltage(v_control, v_control);// arguments: 1V_CONTROL, 1V25_CONTROL
     int mode_control(mode); 
@@ -110,6 +114,9 @@ class TC_PSFE
     static uint16_t saved_pot_value; // saved value of potentiometer, during operation
     static bool is_initialized; // initialization of test card only occurs once
     float ADC_VREF=1.25; // Vref for ADC
+    //Card Id// Full after creating a TC_PSFE object
+    static std::string product_string; // Product string
+    //       //
     const static uint8_t antenna_mask=0x0C; // mask for bits/switches corresponding to antenna channel control - the rest are for voltage control
     const static int fTempLookUpTableSize = 34;
     float fTempLookUpTable[fTempLookUpTableSize] = {848, 804, 755, 701, 643, 584, 525, // 848 corresponds to -40 deg C, steps every 5 degree
@@ -125,12 +132,11 @@ class TC_PSROH
     public:
     enum measurement:char{THERM_SENSE,L_SSA_RST,L_MPA_RST,L_CIC_RST,R_CIC_RST,R_MPA_RST,R_SSA_RST,
                             _3V3,_2V55,_1V25,_1V25_REF,GROUND,_625mV_REF,ISEN_2V55,ISEN_3V3,ISEN_1V25};
-    //Card Id// Full after creating a TC_PSFE object
-    //char product_string[64]; // Product string
-    static std::string product_string;
-    //       //
+    
     TC_PSROH();
+    TC_PSROH(uint32_t,uint8_t); // constructor for multi usb applications // arguments : bus, device number (lsusb) // can be called only once and then revert to the empty one
     ~TC_PSROH();
+    const char* get_product_string(); // pointer to the ID product
     int adc_get (measurement,float&);
     int dac_output(uint16_t); // range: 0-4095  (0-1.25V)
     int toggle_led();
@@ -144,9 +150,16 @@ class TC_PSROH
     static CP2130 cCP2130; // declare CP2130 object    
     bool test_led_state=0;
     static bool fuse_active;
+    //Card Id// Full after creating a TC_PSROH object
+    //char product_string[64]; // Product string
+    static std::string product_string;
+    //       //
     float ADC_VREF=1.25; // Vref for ADC
     static bool is_initialized; // initialization of test card only occurs once
 };
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%//
+
 
 
 

commit 6c8185cf441b193422f06e688eb791c2707db09c

fixed bug on psrohtc i2c communication, introduced with commit 18530b13

Makefile
@@ -17,7 +17,7 @@ all: $(Objs)
 	mv libPh2_TCUSB.so lib
 	#g++ -g -std=c++11 2sseh.cc -Llib -lPh2_TCUSB -o 2sseh.out
 	#g++ -g -std=c++11 psfeh.cc -Llib -lPh2_TCUSB -o psfeh.out
-	#g++ -g -std=c++11 psroh.cc -Llib -lPh2_TCUSB -o psroh.out
+	#g++ -g -std=c++11 psroh.cc -Llib -lPh2_TCUSB -lusb-1.0 -o psroh.out
 	#g++ -g -std=c++11 2sfeh_v1.cc -Llib -lPh2_TCUSB -o 2sfeh_v1.out
 	#g++ -g -std=c++11 half_skeleton.cc -Llib -lPh2_TCUSB -o half_skeleton.out
 

USB_a.cc
@@ -1083,9 +1083,9 @@ int CP2130::configure_spi(cs_line c, device d)
         int result2 = usb_control_msg (fUsbHandle, 0x40, 0x33, 0, 0, spi_delay, sizeof(spi_delay), fUsbTimeout);
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
+        break;
 	case ATSAMD51P20A0A_PSPOH:
 	data[1]=0b00101101; break;
-        break;
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
     int result = usb_control_msg (fUsbHandle, 0x40, 0x31, 0, 0,  data , sizeof (data), fUsbTimeout );

psroh.cc
@@ -29,17 +29,6 @@ std::vector<std::pair<int,int>> read_lpgbt_min_config(std::string file)
 int main(){
 
 	TC_PSROH cTC_PSROH;
-	std::cout <<cTC_PSROH.read_i2c(0x020)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x021)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x022)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x023)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x024)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x029)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x02C)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x031)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x035)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x0EF)<<"\n";
-	std::cout <<cTC_PSROH.read_i2c(0x1C7)<<"\n";
 
 	/*
 	auto cRegs = read_lpgbt_min_config("lpgbt_minimal_config.txt");
@@ -389,6 +378,8 @@ cTC_PSROH.write_i2c(0x110,0xA3);//toggle_2v5 magic number
 	//cTC_PSROH.read_i2c(0x176);
 	//I2C MASTERS CKECK END//
 	*/
+
+	/*
 	//FCMD check//
 
 	cTC_PSROH.write_i2c(0x11D,0xFF);
@@ -412,6 +403,7 @@ cTC_PSROH.write_i2c(0x110,0xA3);//toggle_2v5 magic number
 	cTC_PSROH.write_i2c(0x078,0b00111011);
 	cTC_PSROH.write_i2c(0x082,0b01111011);
 
+	*/
 /*
 
 	cTC_PSROH.dac_output(0);