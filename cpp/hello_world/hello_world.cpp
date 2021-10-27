c800ba5df4dbdf1b4db3308c53877f9e90f54d59
Zpissaki <zefy@hybridtesting2.dyndns.cern.ch>
2021-10-27 09:51:26 +0200

    edited Makefile

--- cpp/hello_world/hello_world.cpp
+++ cpp/hello_world/hello_world.cpp
@@ -1,20 +1,57 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
+// #include <string>
+
+// using namespace std;
+
+// int main()
+// {
+//     vector<string> msg {"Hello\\", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+//     unsigned int ageUtilisateur(26), nombreAmis(10);
+//     string nomUtilisateur ("Kevin");
+//     for (const string& word : msg)
+//     {
+//         cout << word << " ";
+//     }
+//     cout << endl << nomUtilisateur << " " << ageUtilisateur << " " << nombreAmis;
+//     cout << endl;
+    
+//     return 0;
+// }
+
+#include<iostream>
 #include <string>
 
 using namespace std;
 
+class PSPOH
+{
+  private:
+  const char var;
+  
+  public:
+  PSPOH(int constant_value):var(constant_value)
+  {
+   // cout << "Value is " << var << " \n";
+  }
+}; //end class
+
 int main()
 {
-    vector<string> msg {"Hello\\", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    unsigned int ageUtilisateur(26), nombreAmis(10);
-    string nomUtilisateur ("Kevin");
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    
+  int runs;
+  cin >> runs; //select number of sample runs
+  
+ 
+   for (int i = 1; i <= runs; ++i) {
+     cout << i << " ";
+     //PSPOH myobject('h');
+     string msg;
+     msg = "this is the test command\n";
+     cout << msg;
+     
     }
-    cout << endl << nomUtilisateur << " " << ageUtilisateur << " " << nombreAmis;
-    cout << endl;
     
     return 0;
-}
+}//end main
+
